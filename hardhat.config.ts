425d09a06478f58e2b8c866dcbf9e86d75d68d65
Nick Barry <itsnickbarry@protonmail.ch>
2025-06-02 12:37:40 -0600

    configure knip to ignore solidstate contracts dependency

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,4 @@
 import HardhatAbiExporter from './src/index.js';
-// TODO: fix knip hardhat plugin to detect dependenciesToCompile imports
-import '@solidstate/contracts';
 import type { HardhatUserConfig } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
