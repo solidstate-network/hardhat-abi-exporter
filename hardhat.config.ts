b4b1a322a5855ac01c4913353928e2ade081e41e
Nick Barry <itsnickbarry@protonmail.ch>
2025-06-02 12:32:25 -0600

    use solidstate contracts package, import contract for compilation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,14 @@
 import HardhatAbiExporter from './src/index.js';
+// TODO: fix knip hardhat plugin to detect dependenciesToCompile imports
+import '@solidstate/contracts';
 import type { HardhatUserConfig } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
   plugins: [HardhatAbiExporter],
+  solidity: {
+    version: '0.8.30',
+    dependenciesToCompile: ['@solidstate/contracts/interfaces/IERC20.sol'],
+  },
 };
 
 export default config;
